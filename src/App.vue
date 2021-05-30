ac4c8a5f1ea18f4ec6f870f9fa3d6ae07b25b039
TIMM SNOW <two.tone.snow@gmail.com>
2021-05-30 17:41:57 -0400

    creates new base for ike-hawaii frontend, connects axios, sets base-url, adds sign up modal

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="#page-top"><img src="assets/img/ikelogo-small.jpg" alt="..." /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+            <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+            <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
+            <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <router-view />
+    <footer class="footer py-4">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-4 text-lg-start">
+            Copyright &copy; Your Website
+            <!-- This script automatically adds the current year to your website footer-->
+            <!-- (credit: https://updateyourfooter.com/)-->
+          </div>
+          <div class="col-lg-4 my-3 my-lg-0">
+            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+          <div class="col-lg-4 text-lg-end">
+            <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
+            <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,116 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header class="masthead">
+      <div class="container">
+        <div class="masthead-subheading">Welcome...</div>
+        <div class="masthead-heading text-uppercase">'ike Hawaii</div>
+        <a class="portfolio-link btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" href="#portfolioModal1">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">Sign Up</div>
+          </div>
+        </a>
+        <a class="btn btn-primary btn-xl text-uppercase" href="#">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">Log In</div>
+          </div>
+        </a>
+      </div>
+    </header>
+
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-bs-dismiss="modal">
+            <img src="assets/img/close-icon.svg" alt="Close modal" />
+          </div>
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-8">
+                <div class="modal-body">
+                  <!-- Project details-->
+                  <h2 class="text-uppercase">'ike hawaii'</h2>
+                  <p class="item-intro text-muted">IKE: to see, to know, to understand...</p>
+                  <form id="myDIV" v-on:submit.prevent="submit()">
+                    <ul>
+                      <li class="text-danger" v-for="error in errors" v-bind:key="error">
+                        {{ error }}
+                      </li>
+                    </ul>
+                    <div class="form-group">
+                      <!-- <label>Name:</label> -->
+                      <input type="text" class="form-control" v-model="name" placeholder="name" />
+                    </div>
+                    <div class="form-group">
+                      <!-- <label>Email:</label> -->
+                      <input type="email" class="form-control" v-model="email" placeholder="email" />
+                    </div>
+                    <div class="form-group">
+                      <!-- <label>Password:</label> -->
+                      <input type="password" class="form-control" v-model="password" placeholder="password" />
+                    </div>
+                    <div class="form-group">
+                      <!-- <label>Password confirmation:</label> -->
+                      <input
+                        type="password"
+                        class="form-control"
+                        v-model="passwordConfirmation"
+                        placeholder="re-enter password"
+                      />
+                    </div>
+                    <input
+                      type="submit"
+                      class="btn-primary"
+                      style="background-color: rgb(248, 212, 52) !important"
+                      value="Sign Up"
+                    />
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to 'IKE HAWAII'",
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: [],
+    };
+  },
+  created: function () {},
+  methods: {
+    submit: function () {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation,
+      };
+      axios
+        .post("/api/users", params)
+        .then((response) => {
+          console.log(response.data);
+          // this.$router.push("/login");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+    showForm: function () {
+      console.log(this.x);
+      return (this.x = "x");
+    },
   },
 };
 </script>
