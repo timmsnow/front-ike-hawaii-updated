524fca7823ca20382d5b45a51528d1c28ae0fb95
TIMM SNOW <two.tone.snow@gmail.com>
2021-05-31 21:43:41 -0400

    trying to fix routes in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
+  devServer: { historyApiFallback: true },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint",
