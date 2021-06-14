1bbc90e4ab2ec321cdb6a70de8680316da6e88f6
TIMM SNOW <two.tone.snow@gmail.com>
2021-06-14 13:09:26 -0400

    creates home link on app.js

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand" href="#page-top">
+        <a class="navbar-brand" href="/">
           <!-- <img src="assets/img/ikelogo-small.jpg" alt="..." /> -->
           'Ike Hawaii
         </a>
